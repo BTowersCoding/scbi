459c6e92a8f045e19dce8a0e00c0e92c0865bc38
Bobby Towers <porkostomus@mail.com>
2022-01-26 02:36:17 -0800

    resize grid

--- src/main/scbi/app.cljs
+++ src/main/scbi/app.cljs
@@ -85,9 +85,7 @@
       [:line {:stroke "white"
               :stroke-width 10 :pointer-events "none"
               :x1 65 :y1 25
-              :x2 85 :y2 25}]
-      ]]
-    ]
+              :x2 85 :y2 25}]]]]
        [:br]
        [:div
         [:textarea
@@ -99,81 +97,81 @@
 (defn app []
   [:div#app
    [:div#grid
-    [:svg {:width   "50%" :viewBox "-1 -1 702 1102"}
+    [:svg {:width   "60%" :viewBox "-1 -1 702 1102"}
      (into [:g]
            (for [x (range 7) y (range 11)]
-             [:rect {:x      (* x 100) :y (* y 100)
-                     :width  100 :height 100
+             [:rect {:x      (* x 95) :y (* y 95)
+                     :width  95 :height 95
                      :fill   "none" :stroke "black"}]))
      (into [:g {:transform "translate(10,10)"}] factories/metal)
-     (into [:g {:transform "translate(110,10)"}] factories/wood)
-     (into [:g {:transform "translate(210,10)"}] factories/plastic)
-     (into [:g {:transform "translate(310,10)"}] factories/seeds)
-     (into [:g {:transform "translate(410,10)"}] factories/minerals)
-     (into [:g {:transform "translate(510,10)"}] factories/chemicals)
-     (into [:g {:transform "translate(10,110)"}] factories/textiles)
-     (into [:g {:transform "translate(110,110)"}] factories/sugar-and-spices)
-     (into [:g {:transform "translate(210,110)"}] factories/glass)
-     (into [:g {:transform "translate(310,110)"}] factories/animal-feed)
-     (into [:g {:transform "translate(410,110)"}] factories/ic)
-     (into [:g {:transform "translate(10,210)"}] stores/nails)
-     (into [:g {:transform "translate(110,210)"}] stores/planks)
-     (into [:g {:transform "translate(210,210)"}] stores/bricks)
-     (into [:g {:transform "translate(310,210)"}] stores/cement)
-     (into [:g {:transform "translate(410,210)"}] stores/glue)
-     (into [:g {:transform "translate(510,210)"}] stores/paint)
-     (into [:g {:transform "translate(10,310)"}]  stores/hammer)
-     (into [:g {:transform "translate(110,310)"}] stores/measuring-tape)
-     (into [:g {:transform "translate(210,310)"}] stores/shovel)
-     (into [:g {:transform "translate(310,310)"}] stores/cooking-utensils)
-     (into [:g {:transform "translate(410,310)"}] stores/ladder)
-     (into [:g {:transform "translate(510,310)"}] stores/drill)
-     (into [:g {:transform "translate(10,410)"}]  stores/vegetables)
-     (into [:g {:transform "translate(110,410)"}] stores/flour-bag)
-     (into [:g {:transform "translate(210,410)"}] stores/fruit-and-berries)
-     (into [:g {:transform "translate(310,410)"}] stores/cream)
-     (into [:g {:transform "translate(410,410)"}] stores/corn)
-     (into [:g {:transform "translate(510,410)"}] stores/cheese)
-     (into [:g {:transform "translate(610,410)"}] stores/beef)
-     (into [:g {:transform "translate(10,510)"}]  stores/chair)
-     (into [:g {:transform "translate(110,510)"}] stores/tables)
-     (into [:g {:transform "translate(210,510)"}] stores/home-textiles)
-     (into [:g {:transform "translate(310,510)"}] stores/cupboard)
-     (into [:g {:transform "translate(410,510)"}] stores/couch)
-     (into [:g {:transform "translate(10,610)"}]  stores/grass)
-     (into [:g {:transform "translate(110,610)"}] stores/tree-saplings)
-     (into [:g {:transform "translate(210,610)"}] stores/garden-furniture)
-     (into [:g {:transform "translate(310,610)"}] stores/fire-pit)
-     (into [:g {:transform "translate(410,610)"}] stores/lawn-mower)
-     (into [:g {:transform "translate(510,610)"}] stores/garden-gnomes)
-     (into [:g {:transform "translate(10,710)"}] stores/donuts)
-     (into [:g {:transform "translate(110,710)"}] stores/green-smoothie)
-     (into [:g {:transform "translate(210,710)"}] stores/bread-roll)
-     (into [:g {:transform "translate(310,710)"}] stores/cherry-cheesecake)
-     (into [:g {:transform "translate(410,710)"}] stores/frozen-yogurt)
-     (into [:g {:transform "translate(510,710)"}] stores/coffee)
-     (into [:g {:transform "translate(10,810)"}] stores/cap)
-     (into [:g {:transform "translate(110,810)"}] stores/shoes)
-     (into [:g {:transform "translate(210,810)"}] stores/watch)
-     (into [:g {:transform "translate(310,810)"}] stores/business-suits)
-     (into [:g {:transform "translate(410,810)"}] stores/backpack)
-     (into [:g {:transform "translate(10,910)"}] stores/ice-cream-sandwich)
-     (into [:g {:transform "translate(110,910)"}] stores/pizza)
-     (into [:g {:transform "translate(210,910)"}] stores/burgers)
-     (into [:g {:transform "translate(310,910)"}] stores/cheese-fries)
-     (into [:g {:transform "translate(410,910)"}] stores/lemonade-bottle)
-     (into [:g {:transform "translate(510,910)"}] stores/popcorn)
-     (into [:g {:transform "translate(10,1010)"}] stores/bbq-grill)
-     (into [:g {:transform "translate(110,1010)"}] stores/refrigerator)
-     (into [:g {:transform "translate(210,1010)"}] stores/lighting-system)
-     (into [:g {:transform "translate(310,1010)"}] stores/tv)
-     (into [:g {:transform "translate(410,1010)"}] stores/microwave)
+     (into [:g {:transform "translate(105,10)"}] factories/wood)
+     (into [:g {:transform "translate(200,10)"}] factories/plastic)
+     (into [:g {:transform "translate(295,10)"}] factories/seeds)
+     (into [:g {:transform "translate(390,10)"}] factories/minerals)
+     (into [:g {:transform "translate(485,10)"}] factories/chemicals)
+     (into [:g {:transform "translate(10,105)"}] factories/textiles)
+     (into [:g {:transform "translate(105,105)"}] factories/sugar-and-spices)
+     (into [:g {:transform "translate(200,105)"}] factories/glass)
+     (into [:g {:transform "translate(295,105)"}] factories/animal-feed)
+     (into [:g {:transform "translate(390,105)"}] factories/ic)
+     (into [:g {:transform "translate(10,205)"}] stores/nails)
+     (into [:g {:transform "translate(105,205)"}] stores/planks)
+     (into [:g {:transform "translate(200,205)"}] stores/bricks)
+     (into [:g {:transform "translate(295,205)"}] stores/cement)
+     (into [:g {:transform "translate(390,205)"}] stores/glue)
+     (into [:g {:transform "translate(485,205)"}] stores/paint)
+     (into [:g {:transform "translate(10,295)"}]  stores/hammer)
+     (into [:g {:transform "translate(105,295)"}] stores/measuring-tape)
+     (into [:g {:transform "translate(200,295)"}] stores/shovel)
+     (into [:g {:transform "translate(295,295)"}] stores/cooking-utensils)
+     (into [:g {:transform "translate(390,295)"}] stores/ladder)
+     (into [:g {:transform "translate(485,295)"}] stores/drill)
+     (into [:g {:transform "translate(10,395)"}]  stores/vegetables)
+     (into [:g {:transform "translate(105,395)"}] stores/flour-bag)
+     (into [:g {:transform "translate(200,395)"}] stores/fruit-and-berries)
+     (into [:g {:transform "translate(295,395)"}] stores/cream)
+     (into [:g {:transform "translate(390,395)"}] stores/corn)
+     (into [:g {:transform "translate(485,395)"}] stores/cheese)
+     (into [:g {:transform "translate(580,395)"}] stores/beef)
+     (into [:g {:transform "translate(10,490)"}]  stores/chair)
+     (into [:g {:transform "translate(105,490)"}] stores/tables)
+     (into [:g {:transform "translate(200,490)"}] stores/home-textiles)
+     (into [:g {:transform "translate(295,490)"}] stores/cupboard)
+     (into [:g {:transform "translate(390,490)"}] stores/couch)
+     (into [:g {:transform "translate(10,585)"}]  stores/grass)
+     (into [:g {:transform "translate(105,585)"}] stores/tree-saplings)
+     (into [:g {:transform "translate(200,585)"}] stores/garden-furniture)
+     (into [:g {:transform "translate(295,585)"}] stores/fire-pit)
+     (into [:g {:transform "translate(390,585)"}] stores/lawn-mower)
+     (into [:g {:transform "translate(485,585)"}] stores/garden-gnomes)
+     (into [:g {:transform "translate(10,680)"}] stores/donuts)
+     (into [:g {:transform "translate(105,680)"}] stores/green-smoothie)
+     (into [:g {:transform "translate(200,680)"}] stores/bread-roll)
+     (into [:g {:transform "translate(295,680)"}] stores/cherry-cheesecake)
+     (into [:g {:transform "translate(390,680)"}] stores/frozen-yogurt)
+     (into [:g {:transform "translate(485,680)"}] stores/coffee)
+     (into [:g {:transform "translate(10,770)"}] stores/cap)
+     (into [:g {:transform "translate(105,770)"}] stores/shoes)
+     (into [:g {:transform "translate(200,770)"}] stores/watch)
+     (into [:g {:transform "translate(295,770)"}] stores/business-suits)
+     (into [:g {:transform "translate(390,770)"}] stores/backpack)
+     (into [:g {:transform "translate(10,865)"}] stores/ice-cream-sandwich)
+     (into [:g {:transform "translate(105,865)"}] stores/pizza)
+     (into [:g {:transform "translate(200,865)"}] stores/burgers)
+     (into [:g {:transform "translate(295,865)"}] stores/cheese-fries)
+     (into [:g {:transform "translate(390,865)"}] stores/lemonade-bottle)
+     (into [:g {:transform "translate(485,865)"}] stores/popcorn)
+     (into [:g {:transform "translate(10,955)"}] stores/bbq-grill)
+     (into [:g {:transform "translate(105,955)"}] stores/refrigerator)
+     (into [:g {:transform "translate(200,955)"}] stores/lighting-system)
+     (into [:g {:transform "translate(295,955)"}] stores/tv)
+     (into [:g {:transform "translate(390,955)"}] stores/microwave)
 
      ;mouse targets --up
      (into [:g]
            (for [x (range 7) y (range 11)]
-             [:rect {:x      (* x 100) :y      (* y 100)
-                     :width  100 :height 50
+             [:rect {:x      (* x 95) :y      (* y 95)
+                     :width  95 :height 47.5
                      :visibility "hidden"
                      :pointer-events "all"
                      :on-mouse-over #(reset! hover [x y :up])
@@ -183,8 +181,8 @@
       ;mouse targets --down
      (into [:g]
            (for [x (range 7)  y (range 11)]
-             [:rect {:x              (* x 100) :y              (+ 50 (* y 100))
-                     :width          100  :height         50
+             [:rect {:x              (* x 95) :y              (+ 47.5 (* y 95))
+                     :width          95  :height         47.5
                      :visibility     "hidden"
                      :pointer-events "all"
                      :on-mouse-over  #(reset! hover [x y :down])
@@ -196,7 +194,7 @@
        (for [x (range 7)  y (range 11)
              :when (= @hover [x y :up])]
         [:path.fade {:d "M0 21.5H9V39H32V21.5H41L20.5 0" :fill "#0C0"
-                     :transform (str "translate(" (+ 25 (* x 100)) "," (* y 100) ")")
+                     :transform (str "translate(" (+ 22.5 (* x 95)) "," (* y 95) ")")
                      :pointer-events "none"}]))
 
      ;down arrows
@@ -204,7 +202,7 @@
            (for [x (range 7)  y (range 11)
                   :when (= @hover [x y :down])]
              [:path.fade {:d "M9 0v17.5h-9l20.5 21.5 20.5-21.5h-9V0z" :fill "#f00"
-                          :transform (str "translate(" (+ 25 (* x 100)) "," (+ 60 (* y 100)) ")")
+                          :transform (str "translate(" (+ 22.5 (* x 95)) "," (+ 60 (* y 95)) ")")
                           :pointer-events "none"}]))]
      
 
